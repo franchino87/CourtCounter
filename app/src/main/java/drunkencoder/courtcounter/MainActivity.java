fc7ed674dee780bac072d36a036f8d1e2e39d2dc
Panella <panca.boy@tiscali.it>
2015-06-22 23:53:51 +0200

    Court Counter Final Version
    Court Counter Final Version

--- app/src/main/java/drunkencoder/courtcounter/MainActivity.java
+++ app/src/main/java/drunkencoder/courtcounter/MainActivity.java
@@ -1,9 +1,11 @@
 package drunkencoder.courtcounter;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,4 +37,44 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void click_3pointsA (View view) {
+        displayA(3);
+    }
+    public void click_2pointsA (View view) {
+        displayA(2);
+    }
+    public void click_1pointsA (View view) {
+        displayA(1);
+    }
+    public void click_3pointsB (View view) {
+        displayB(3);
+    }
+    public void click_2pointsB (View view) {
+        displayB(2);
+    }
+    public void click_1pointsB (View view) {
+        displayB(1);
+    }
+
+    public void click_reset (View view){
+        TextView textViewA = (TextView) findViewById(R.id.scoreA);
+        textViewA.setText("" + 0);
+        TextView textViewB = (TextView) findViewById(R.id.scoreB);
+        textViewB.setText("" + 0);
+    }
+
+
+    private void displayA(int number){
+        TextView textView = (TextView) findViewById(R.id.scoreA);
+        number = number + (Integer.parseInt(textView.getText().toString()));
+        textView.setText("" + number);
+    }
+
+    private void displayB(int number){
+        TextView textView = (TextView) findViewById(R.id.scoreB);
+        number = number + (Integer.parseInt(textView.getText().toString()));
+        textView.setText("" + number);
+    }
+
 }
